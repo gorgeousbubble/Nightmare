367dd70e441cca03470223cc091658dfba176431
AuroraShine <hyx1029535012@icloud.com>
2019-11-27 13:28:32 +0800

    Add route handle function.

--- nets/nets_http_flask.py
+++ nets/nets_http_flask.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return 'hello,world!'
+
+
+@app.route('/nightmare', methods=['GET'])
+def home():
+    return 'Nightmare Project'
 
 
 if __name__ == '__main__':
