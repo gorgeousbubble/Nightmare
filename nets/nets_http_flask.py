cdce754d11d0fce2bd356eb3d97cbc4dbf1db92d
AuroraShine <hyx1029535012@icloud.com>
2019-11-28 15:47:49 +0800

    Add nets http flask print information.

--- nets/nets_http_flask.py
+++ nets/nets_http_flask.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import argparse
 from flask import Flask
 
 app = Flask(__name__)
@@ -16,5 +17,11 @@ def home():
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-i', '--ip', help='ip address: ipv4 address witch http flask server listen, such as \'0.0.0.0\'', type=str, default='0.0.0.0')
+    parser.add_argument(
+        '-p', '--port', help='port: port number witch http flask server listen, such as \'5000\'', type=int, default=5000)
+    args = parser.parse_args()
+    print('Start Listen And Server on {}:{}'.format(args.ip, args.port))
+    app.run(host=args.ip, port=args.port, debug=True)
