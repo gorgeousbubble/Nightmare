055ad08f8e8ca5e8bdc13c05e04c6eb9d7fd8722
Alopex6414 <1029535012@qq.com>
2019-12-04 23:26:18 +0800

    Create update.bat

---
+++ update.bat
@@ -0,0 +1,6 @@
+:: Nightmare Project - Git Update(Github)
+:: Copyright(C) 2019, Team Gorgeous Bubble, All Rights Reserved.
+@echo off
+
+:: Git Automatic Update with master branch
+git pull origin master
