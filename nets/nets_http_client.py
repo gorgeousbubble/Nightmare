28c0154164924bb3cfb18b4a4ce9b0f564f03ba4
AuroraShine <hyx1029535012@icloud.com>
2020-01-09 10:48:23 +0800

    [improve]Change nets http client methods.

--- nets/nets_http_client.py
+++ nets/nets_http_client.py
@@ -2,20 +2,55 @@
 # -*- coding: utf-8 -*-
 import argparse
 import http.client
+import time
 
 
 class HttpClient(object):
-    def __init__(self, host='localhost', port=8080):
-        self.Host = host
-        self.Port = port
-        self.Addr = (self.Host, self.Port)
-        self.Conn = http.client.HTTPConnection(host=self.Host, port=self.Port)
+    def __init__(self, host, port, timeout=5, retry=3):
+        self.host = host
+        self.port = port
+        self.timeout = timeout
+        self.retry = retry
+        self.addr = (self.host, self.port)
 
     def request(self, *args, **kwargs):
-        self.Conn.request(*args, **kwargs)
+        # http response message
+        response = None
+        try:
+            # connect to http server
+            conn = http.client.HTTPConnection(host=self.host, port=self.port, timeout=self.timeout)
+            # send http request
+            conn.request(*args, **kwargs)
+            # fetch http response
+            response = conn.getresponse()
+        except Exception as e:
+            print(e)
+        finally:
+            return response
 
-    def response(self):
-        response = self.Conn.getresponse()
+    def request_with_retry(self, *args, **kwargs):
+        # connect to http server
+        conn = http.client.HTTPConnection(host=self.host, port=self.port, timeout=self.timeout)
+        # send http request and retry
+        while True:
+            try:
+                # send request when response 200 return
+                conn.request(*args, **kwargs)
+                response = conn.getresponse()
+                if response.status == http.HTTPStatus.OK:
+                    break
+            except Exception as e:
+                # response error
+                response = e
+                pass
+            self.retry -= 1
+            time.sleep(1)
+            if self.retry > 0:
+                time.sleep(1)
+            else:
+                break
+        if isinstance(response, Exception):
+            raise response
         return response
 
 
@@ -27,5 +62,4 @@ if __name__ == '__main__':
         '-p', '--port', help='port: port number witch http client connect, such as \'8080\'', type=int, default=8080)
     args = parser.parse_args()
     c = HttpClient(host=args.ip, port=args.port)
-    c.request(method='GET', url='/')
-    print(c.response())
+    print(c.request(method='GET', url='/'))
