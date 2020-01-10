1b907657b6522d29a8efcbb75512448d6125bf3c
AuroraShine <hyx1029535012@icloud.com>
2020-01-10 11:22:11 +0800

    [improve]format commands tips information.

--- cmds/cmd_udp.py
+++ cmds/cmd_udp.py
@@ -7,9 +7,12 @@ from nets import start_udp_client
 
 def parse_cmd_udp(cmd_parser, sub_parser):
     # parse sub parameters
-    sub_parser.add_argument('-i', '--ip', help='ip address: ipv4 address witch tcp server listen, such as \'127.0.0.1\'', type=str, default='127.0.0.1')
-    sub_parser.add_argument('-p', '--port', help='port: port number witch tcp server listen, such as \'6001\'', type=int, default=6001)
-    sub_parser.add_argument('-m', '--mode', help='mode: udp mode choose, \'s\' or \'server\' indicate udp server, \'c\' or \'client\' indicate udp client', type=str, default='server')
+    sub_parser.add_argument('-i', '--ip', help='ip address: ipv4 address witch tcp server listen',
+                            type=str, default='127.0.0.1')
+    sub_parser.add_argument('-p', '--port', help='port: port number witch tcp server listen',
+                            type=int, default=6001)
+    sub_parser.add_argument('-m', '--mode', help='mode: udp mode choose, \'server\' or \'client\' indicate udp client',
+                            type=str, default='server')
     args = cmd_parser.parse_args()
     print(args)
     # choose udp mode
