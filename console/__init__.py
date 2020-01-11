9f7f617079d0281c1b1779e3e7d14f1f686987c5
AuroraShine <hyx1029535012@icloud.com>
2020-01-11 16:37:57 +0800

    [test]Append console test code.

--- console/__init__.py
+++ console/__init__.py
@@ -1,2 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import console
+from console.console import Console
+from console.console import ConsoleLevel
---
+++ console/console_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import unittest
+from console import Console
+from console import ConsoleLevel
+
+
+class TestConsole(unittest.TestCase):
+    def test_simple_print(self):
+        Console.debug('hello,world')
+
+    def test_level_print(self):
+        Console.level = ConsoleLevel.INFO
+        Console.debug('hello,world')
+        Console.info('python')
+
+    def test_disable_print(self):
+        Console.enable = False
+        Console.debug('hello,world')
+        Console.info('python')
