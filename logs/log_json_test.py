bdb7aa98973bdd3b99c1a1e752b2f9ecb003961c
Alopex6414 <1029535012@qq.com>
2020-02-12 13:53:17 +0800

    [test]Create logs test json.

---
+++ logs/log_json_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import logging
+import unittest
+from logs.log_json import LogJson
+
+
+class TestLogJson(unittest.TestCase):
+    def test_init(self):
+        log = LogJson(file='./json/log.json', path='../log').get_logger(__name__)
+        log.debug('hello,world~')
