eee1f5261ced1dc15918f75a2e67a1cb2bc7cc4f
Alopex6414 <1029535012@qq.com>
2020-02-13 10:38:35 +0800

    [test]Append test code of logs yaml.

---
+++ logs/log_yaml_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import logging
+import unittest
+from logs.log_yaml import LogYaml
+
+
+class TestLogYaml(unittest.TestCase):
+    def test_init(self):
+        log = LogYaml(file='./yaml/log.yaml', path='../log').get_logger(__name__)
+        log.debug('hello,world~')
