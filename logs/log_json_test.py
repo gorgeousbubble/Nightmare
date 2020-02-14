0a69e05c9d72125f4d382f4a714b7da38cc3e315
Alopex6414 <1029535012@qq.com>
2020-02-14 11:52:53 +0800

    [feature]Append test code.

--- logs/log_json_test.py
+++ logs/log_json_test.py
@@ -9,3 +9,4 @@ class TestLogJson(unittest.TestCase):
     def test_init(self):
         log = LogJson(file='./json/log.json', path='../log').get_logger(__name__)
         log.debug('hello,world~')
+        log.info('python json.')
