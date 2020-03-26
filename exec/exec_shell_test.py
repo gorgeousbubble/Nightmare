5447c7a9ea9515e10bbd6f316d485ac2d0413599
AuroraShine <hyx1029535012@icloud.com>
2020-03-26 10:33:43 +0800

    add test code

--- exec/exec_shell_test.py
+++ exec/exec_shell_test.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import unittest
+from exec.exec_shell import exec_shell
+
+
+class TestExecShell(unittest.TestCase):
+    def test_exec_echo(self):
+        pass
