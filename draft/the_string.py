c240bb9649c4d980d9e3d4b0936ee1dd915dddd9
AuroraShine <hyx1029535012@icloud.com>
2020-03-31 13:35:53 +0800

    [feature]Update code

--- draft/the_string.py
+++ draft/the_string.py
@@ -16,3 +16,5 @@ if __name__ == '__main__':
     print('string combine:')
     print("'hello,%s'%'world!':", 'hello,%s' % 'world!')
     print("'hi %s, you have $%d in bank.' % ('alopex', 100000000):",'hi %s, you have $%d in bank.' % ('alopex', 100000000))
+    print('Today is %04d-%02d-%02d, time now: %2d:%02d pi is %f, symbol %%' % (2020, 2, 24, 3, 9, 3.1415926))
+    print('We can also use \'format\' function to format str like this:', 'nice to meet you! {}'.format('erika'))
