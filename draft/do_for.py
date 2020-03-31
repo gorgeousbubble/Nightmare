2ff53f633c7f54ab286d2ddb1afc81d87a11a81d
AuroraShine <hyx1029535012@icloud.com>
2020-03-31 15:13:35 +0800

    [feature]Update code

---
+++ draft/do_for.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+if __name__ == '__main__':
+    print('print sum from 1~100:')
+    sum = 0
+    for v in range(101):
+        sum += v
+    print(sum)
