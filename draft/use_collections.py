d6d63fc602ee79e4a008c07adca6e864a31509f6
AuroraShine <hyx1029535012@icloud.com>
2020-04-11 14:37:44 +0800

    [feature]Update draft code

--- draft/use_collections.py
+++ draft/use_collections.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from collections import namedtuple
+from collections import deque
 
 
 def test_namedtuple():
@@ -11,5 +12,13 @@ def test_namedtuple():
     print('isinstance(p, tuple):', isinstance(p, tuple))
 
 
+def test_deque():
+    q = deque(['A', 'B', 'C'])
+    q.append('X')
+    q.appendleft('Y')
+    print('q:', q)
+
+
 if __name__ == '__main__':
     test_namedtuple()
+    test_deque()
