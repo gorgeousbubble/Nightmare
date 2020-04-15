090fffe261b52f76084e9f699b2fadc6a7547bef
AuroraShine <hyx1029535012@icloud.com>
2020-04-15 13:51:44 +0800

    [improve]Append draft code

--- draft/use_collections.py
+++ draft/use_collections.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from collections import namedtuple
 from collections import deque
+from collections import defaultdict
 
 
 def test_namedtuple():
@@ -19,6 +20,14 @@ def test_deque():
     print('q:', q)
 
 
+def test_defaultdict():
+    d = defaultdict(lambda: 'N/A')
+    d['key1'] = 'abc'
+    print('d[\'key1\']:', d['key1'])
+    print('d[\'key2\']:', d['key2'])
+
+
 if __name__ == '__main__':
     test_namedtuple()
     test_deque()
+    test_defaultdict()
