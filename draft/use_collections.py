6f44b66e348e286218d666b74d3fb49298d50c7c
AuroraShine <hyx1029535012@icloud.com>
2020-04-17 17:26:54 +0800

    [improve]Append comments

--- draft/use_collections.py
+++ draft/use_collections.py
@@ -3,6 +3,7 @@
 from collections import namedtuple
 from collections import deque
 from collections import defaultdict
+from collections import OrderedDict
 
 
 def test_namedtuple():
@@ -27,7 +28,13 @@ def test_defaultdict():
     print('d[\'key2\']:', d['key2'])
 
 
+def test_ordereddict():
+    d = OrderedDict([('a', 1), ('b', 2), ('c', 3)])
+    print(d)
+
+
 if __name__ == '__main__':
     test_namedtuple()
     test_deque()
     test_defaultdict()
+    test_ordereddict()
