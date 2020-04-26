d9d7f19381613d067d808cb2766677e96f17877d
AuroraShine <hyx1029535012@icloud.com>
2020-04-26 14:37:07 +0800

    [improve]Alter function to hash.py

--- cmds/cmd_hash.py
+++ cmds/cmd_hash.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from hash import hash_md5, hash_sha1, hash_sha224, hash_sha256, hash_sha512
+from hash import hash_gen
 
 
 def parse_cmd_hash(cmd_parser, sub_parser):
@@ -10,25 +10,5 @@ def parse_cmd_hash(cmd_parser, sub_parser):
     sub_parser.add_argument('-t', '--type', help='algorithm type: calc with which hash algorithms(md5, sha1, sha256)',
                             type=str, default='')
     args = cmd_parser.parse_args()
-    # check input string
-    if args.input == '':
-        print('The input string can not be empty!')
-        return
-    # choose hash algorithm mode
-    if args.type == 'md5':
-        r = hash_md5(args.input)
-        print('calc md5:{}'.format(r))
-    elif args.type == 'sha1':
-        r = hash_sha1(args.input)
-        print('calc sha1:{}'.format(r))
-    elif args.type == 'sha224':
-        r = hash_sha224(args.input)
-        print('calc sha224:{}'.format(r))
-    elif args.type == 'sha256':
-        r = hash_sha256(args.input)
-        print('calc sha256:{}'.format(r))
-    elif args.type == 'sha512':
-        r = hash_sha512(args.input)
-        print('calc sha512:{}'.format(r))
-    else:
-        print('Invalid hash algorithm. You can choose one of hash algorithm from support list.')
+    # generate hash code
+    hash_gen(args.input, args.type)
