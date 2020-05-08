372e94e969279963441fd6bcfbbeb2f2d433638e
AuroraShine <hyx1029535012@icloud.com>
2020-05-08 10:36:38 +0800

    [feature]Create hash_md5_encode function and add test class

--- hash/hash_md5.py
+++ hash/hash_md5.py
@@ -9,6 +9,12 @@ def hash_md5(s):
     return md5.hexdigest()
 
 
+def hash_md5_encode(s):
+    md5 = hashlib.md5()
+    md5.update(s.encode('utf-8'))
+    return md5.hexdigest()
+
+
 def hash_md5_check(s, r):
     md5 = hashlib.md5()
     md5.update(s.encode('utf-8'))
--- hash/hash_md5_test.py
+++ hash/hash_md5_test.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import unittest
+from .hash_md5 import hash_md5_encode, hash_md5_check
+
+
+class TestHashMD5(unittest.TestCase):
+    def test_hash_md5_encode(self):
+        s = 'hello,world!'
+        r = hash_md5_encode(s)
+        print('hash md5 encode:', r)
+
+    def test_hash_md5_check(self):
+        s = 'hello,world!'
+        r = 'c0e84e870874dd37ed0d164c7986f03a'
+        self.assertTrue(hash_md5_check(s, r))
+        print('hash md5 check pass')
