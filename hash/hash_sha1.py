22fec6c018c3fee88cfb9e850eac7abb126cceee
AuroraShine <hyx1029535012@icloud.com>
2020-05-09 10:28:49 +0800

    [feature]Append function sha1 encode

--- hash/hash_sha1.py
+++ hash/hash_sha1.py
@@ -7,3 +7,15 @@ def hash_sha1(s):
     sha1 = hashlib.sha1()
     sha1.update(s.encode('utf-8'))
     return sha1.hexdigest()
+
+
+def hash_sha1_encode(s):
+    sha1 = hashlib.sha1()
+    sha1.update(s.encode('utf-8'))
+    return sha1.hexdigest()
+
+
+def hash_sha1_check(s, r):
+    sha1 = hashlib.sha1()
+    sha1.update(s.encode('utf-8'))
+    return sha1.hexdigest() == r
