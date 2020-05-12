4d352779b7c098c421003dba067ceca977083e0f
AuroraShine <hyx1029535012@icloud.com>
2020-05-12 10:31:03 +0800

    [feature]Append test code

--- hash/hash_sha256_test.py
+++ hash/hash_sha256_test.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import unittest
+from .hash_sha256 import hash_sha256_encode, hash_sha256_check
+
+
+class TestHashSHA256(unittest.TestCase):
+    def test_hash_sha256_encode(self):
+        s = 'hello,world!'
+        r = hash_sha256_encode(s)
+        print('hash sha256 encode:', r)
+
+    def test_hash_sha256_check(self):
+        s = 'hello,world!'
+        r = 'ec1e0bd875226943ad0e8877bdba4ca449c4cb8591a5363921c9f1ee20084c34'
+        self.assertTrue(hash_sha256_check(s, r))
+        print('hash sha256 check pass')
