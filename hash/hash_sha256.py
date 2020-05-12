bcb1748f2e4d082ef6158b971d64b87575e2faf3
AuroraShine <hyx1029535012@icloud.com>
2020-05-12 10:27:30 +0800

    [improve]Add function hash_sha256_encode and hash_sha256_check

--- hash/hash_sha256.py
+++ hash/hash_sha256.py
@@ -7,3 +7,15 @@ def hash_sha256(s):
     sha256 = hashlib.sha256()
     sha256.update(s.encode('utf-8'))
     return sha256.hexdigest()
+
+
+def hash_sha256_encode(s):
+    sha256 = hashlib.sha256()
+    sha256.update(s.encode('utf-8'))
+    return sha256.hexdigest()
+
+
+def hash_sha256_check(s, r):
+    sha256 = hashlib.sha256()
+    sha256.update(s.encode('utf-8'))
+    return sha256.hexdigest() == r
