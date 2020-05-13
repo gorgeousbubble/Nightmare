264a2b52ce8e2949e0146af1b412de6f385ba9d0
AuroraShine <hyx1029535012@icloud.com>
2020-05-13 10:38:59 +0800

    [feature]Create sha384 encode and check function

--- hash/hash_sha384.py
+++ hash/hash_sha384.py
@@ -7,3 +7,15 @@ def hash_sha384(s):
     sha384 = hashlib.sha384()
     sha384.update(s.encode('utf-8'))
     return sha384.hexdigest()
+
+
+def hash_sha384_encode(s):
+    sha384 = hashlib.sha384()
+    sha384.update(s.encode('utf-8'))
+    return sha384.hexdigest()
+
+
+def hash_sha384_check(s, r):
+    sha384 = hashlib.sha384()
+    sha384.update(s.encode('utf-8'))
+    return sha384.hexdigest() == r
