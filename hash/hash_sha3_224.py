e4e9f6c436b1131e4214a05d8512db57d8dc5f78
AuroraShine <hyx1029535012@icloud.com>
2020-05-15 10:54:57 +0800

    [feature]Apppend encode and check function in sha3_224

--- hash/hash_sha3_224.py
+++ hash/hash_sha3_224.py
@@ -7,3 +7,15 @@ def hash_sha3_224(s):
     sha3_224 = hashlib.sha3_224()
     sha3_224.update(s.encode('utf-8'))
     return sha3_224.hexdigest()
+
+
+def hash_sha3_224_encode(s):
+    sha3_224 = hashlib.sha3_224()
+    sha3_224.update(s.encode('utf-8'))
+    return sha3_224.hexdigest()
+
+
+def hash_sha3_224_check(s, r):
+    sha3_224 = hashlib.sha3_224()
+    sha3_224.update(s.encode('utf-8'))
+    return sha3_224.hexdigest() == r
