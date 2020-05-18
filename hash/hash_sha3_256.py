66b17126ba47aefefbc43dd7d32c84c69976b637
AuroraShine <hyx1029535012@icloud.com>
2020-05-18 10:38:37 +0800

    [feature]Append encode and check function

--- hash/hash_sha3_256.py
+++ hash/hash_sha3_256.py
@@ -7,3 +7,15 @@ def hash_sha3_256(s):
     sha3_256 = hashlib.sha3_256()
     sha3_256.update(s.encode('utf-8'))
     return sha3_256.hexdigest()
+
+
+def hash_sha3_256_encode(s):
+    sha3_256 = hashlib.sha3_256()
+    sha3_256.update(s.encode('utf-8'))
+    return sha3_256.hexdigest()
+
+
+def hash_sha3_256_check(s, r):
+    sha3_256 = hashlib.sha3_256()
+    sha3_256.update(s.encode('utf-8'))
+    return sha3_256.hexdigest() == r
