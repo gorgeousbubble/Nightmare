75b59edd609c01212ce322847a1c3a0d442633f4
AuroraShine <hyx1029535012@icloud.com>
2020-05-20 10:08:44 +0800

    [feature]Add sha3 512 test code

--- hash/hash_sha3_512.py
+++ hash/hash_sha3_512.py
@@ -15,7 +15,7 @@ def hash_sha3_512_encode(s):
     return sha3_512.hexdigest()
 
 
-def hash_sha3_512_check(s):
+def hash_sha3_512_check(s, r):
     sha3_512 = hashlib.sha3_512()
     sha3_512.update(s.encode('utf-8'))
     return sha3_512.hexdigest() == r
--- hash/hash_sha3_512_test.py
+++ hash/hash_sha3_512_test.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import unittest
+from .hash_sha3_512 import hash_sha3_512_encode, hash_sha3_512_check
+
+
+class TestHashSHA3512(unittest.TestCase):
+    def test_hash_sha3_512_encode(self):
+        s = 'hello,world!'
+        r = hash_sha3_512_encode(s)
+        print('hash sha3_512 encode:', r)
+
+    def test_hash_sha3_512_check(self):
+        s = 'hello,world!'
+        r = '4ed8dc6739e8edaa481d548e7f93d7a6c63d833a75bcfbd615fd932c97e6f357316e10d488778b12d2b2004341b641c54fa3a7942555bdcc1f672c3bfa578725'
+        self.assertTrue(hash_sha3_512_check(s, r))
+        print('hash sha3_512 check pass')
