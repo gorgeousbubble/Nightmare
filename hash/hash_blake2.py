0f3f754edb0834bd45006e02ba28f06fd329f5d5
AuroraShine <hyx1029535012@icloud.com>
2020-05-21 10:30:21 +0800

    [feature]Append test code

--- hash/hash_blake2.py
+++ hash/hash_blake2.py
@@ -7,3 +7,15 @@ def hash_blake2b(s):
     blake2b = hashlib.blake2b(digest_size=16)
     blake2b.update(s.encode('utf-8'))
     return blake2b.hexdigest()
+
+
+def hash_blake2b_encode(s):
+    blake2b = hashlib.blake2b(digest_size=16)
+    blake2b.update(s.encode('utf-8'))
+    return blake2b.hexdigest()
+
+
+def hash_blake2b_check(s, r):
+    blake2b = hashlib.blake2b(digest_size=16)
+    blake2b.update(s.encode('utf-8'))
+    return blake2b.hexdigest() == r
--- hash/hash_blake2_test.py
+++ hash/hash_blake2_test.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import unittest
+from .hash_blake2 import hash_blake2b_encode, hash_blake2b_check
+
+
+class TestHashBlake2(unittest.TestCase):
+    def test_hash_blake2b_encode(self):
+        s = 'hello,world!'
+        r = hash_blake2b_encode(s)
+        print('hash blake2b encode:', r)
+
+    def test_hash_blake2b_check(self):
+        s = 'hello,world!'
+        r = '1748e3d0f53508245851db4571424eee'
+        self.assertTrue(hash_blake2b_check(s, r))
+        print('hash blake2b check pass')
