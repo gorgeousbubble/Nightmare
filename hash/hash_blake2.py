9be2bd05db65f7d93cd0d0c54c13a0e4496e080a
AuroraShine <hyx1029535012@icloud.com>
2020-05-21 10:26:27 +0800

    [feature]Create blake2 test code and Append funciton

--- hash/hash_blake2.py
+++ hash/hash_blake2.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import hashlib
+
+
+def hash_blake2b(s):
+    blake2b = hashlib.blake2b(digest_size=16)
+    blake2b.update(s.encode('utf-8'))
+    return blake2b.hexdigest()
