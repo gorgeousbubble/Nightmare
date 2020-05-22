f527bce9c2c6fae705a03d0be0bd4cdfc315e8ff
AuroraShine <hyx1029535012@icloud.com>
2020-05-22 09:44:01 +0800

    [feature]Append blake2s function

--- hash/hash_blake2.py
+++ hash/hash_blake2.py
@@ -19,3 +19,21 @@ def hash_blake2b_check(s, r):
     blake2b = hashlib.blake2b(digest_size=16)
     blake2b.update(s.encode('utf-8'))
     return blake2b.hexdigest() == r
+
+
+def hash_blake2s(s):
+    blake2s = hashlib.blake2s(digest_size=16)
+    blake2s.update(s.encode('utf-8'))
+    return blake2s.hexdigest()
+
+
+def hash_blake2s_encode(s):
+    blake2s = hashlib.blake2s(digest_size=16)
+    blake2s.update(s.encode('utf-8'))
+    return blake2s.hexdigest()
+
+
+def hash_blake2s_check(s, r):
+    blake2s = hashlib.blake2s(digest_size=16)
+    blake2s.update(s.encode('utf-8'))
+    return blake2s.hexdigest() == r
