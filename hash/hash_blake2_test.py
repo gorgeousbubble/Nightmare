49a1a110170f32538d073e593d0719c3f0c6e2a0
AuroraShine <hyx1029535012@icloud.com>
2020-05-22 10:14:34 +0800

    [feature]Append blake2 test code

--- hash/hash_blake2_test.py
+++ hash/hash_blake2_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import unittest
-from .hash_blake2 import hash_blake2b_encode, hash_blake2b_check
+from .hash_blake2 import hash_blake2b_encode, hash_blake2b_check, hash_blake2s_encode, hash_blake2s_check
 
 
 class TestHashBlake2(unittest.TestCase):
@@ -15,3 +15,14 @@ class TestHashBlake2(unittest.TestCase):
         r = '1748e3d0f53508245851db4571424eee'
         self.assertTrue(hash_blake2b_check(s, r))
         print('hash blake2b check pass')
+
+    def test_hash_blake2s_encode(self):
+        s = 'hello,world!'
+        r = hash_blake2s_encode(s)
+        print('hash blake2s encode:', r)
+
+    def test_hash_blake2s_check(self):
+        s = 'hello,world!'
+        r = 'fec96d2115ab677ca3e7561ce32100bd'
+        self.assertTrue(hash_blake2s_check(s, r))
+        print('hash blake2s check pass')
