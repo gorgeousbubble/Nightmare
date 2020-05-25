cecf3c47ed20488b5e4fbf15f06a325c299a90b1
AuroraShine <hyx1029535012@icloud.com>
2020-05-25 10:30:41 +0800

    [feature]Append shake 128 test code

--- hash/hash_shake.py
+++ hash/hash_shake.py
@@ -6,16 +6,16 @@ import hashlib
 def hash_shake128(s):
     shake128 = hashlib.shake_128()
     shake128.update(s.encode('utf-8'))
-    return shake128.hexdigest()
+    return shake128.hexdigest(128)
 
 
 def hash_shake128_encode(s):
     shake128 = hashlib.shake_128()
     shake128.update(s.encode('utf-8'))
-    return shake128.hexdigest()
+    return shake128.hexdigest(128)
 
 
 def hash_shake128_check(s, r):
     shake128 = hashlib.shake_128()
     shake128.update(s.encode('utf-8'))
-    return shake128.hexdigest() == r
+    return shake128.hexdigest(128) == r
