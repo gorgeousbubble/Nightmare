343f70aef9d053e3ff5c69bbe1e9294475beafb1
AuroraShine <hyx1029535012@icloud.com>
2020-05-29 10:30:39 +0800

    [feature]Add hash_check function

--- hash/hash.py
+++ hash/hash.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import hashlib
-from hash.hash_md5 import hash_md5
-from hash.hash_sha1 import hash_sha1
-from hash.hash_sha224 import hash_sha224
-from hash.hash_sha256 import hash_sha256
-from hash.hash_sha384 import hash_sha384
-from hash.hash_sha512 import hash_sha512
-from hash.hash_sha3_224 import hash_sha3_224
-from hash.hash_sha3_256 import hash_sha3_256
-from hash.hash_sha3_384 import hash_sha3_384
-from hash.hash_sha3_512 import hash_sha3_512
-from hash.hash_shake import hash_shake128, hash_shake256
-from hash.hash_blake2 import hash_blake2b, hash_blake2s
+from hash.hash_md5 import hash_md5, hash_md5_check
+from hash.hash_sha1 import hash_sha1, hash_sha1_check
+from hash.hash_sha224 import hash_sha224, hash_sha224_check
+from hash.hash_sha256 import hash_sha256, hash_sha256_check
+from hash.hash_sha384 import hash_sha384, hash_sha384_check
+from hash.hash_sha512 import hash_sha512, hash_sha512_check
+from hash.hash_sha3_224 import hash_sha3_224, hash_sha3_224_check
+from hash.hash_sha3_256 import hash_sha3_256, hash_sha3_256_check
+from hash.hash_sha3_384 import hash_sha3_384, hash_sha3_384_check
+from hash.hash_sha3_512 import hash_sha3_512, hash_sha3_512_check
+from hash.hash_shake import hash_shake128, hash_shake256, hash_shake128_check, hash_shake256_check
+from hash.hash_blake2 import hash_blake2b, hash_blake2s, hash_blake2b_check, hash_blake2s_check
 
 
 def hash_gen(s, t):
@@ -63,5 +63,39 @@ def hash_gen(s, t):
     elif t == 'blake2s':
         r = hash_blake2s(s)
         print('calc blake2s:{}'.format(r))
+    else:
+        print('Invalid hash algorithm. You can choose one of hash algorithm from support list.')
+
+
+def hash_check(s, r, t):
+    # choose hash algorithm mode
+    if t == 'md5':
+        print('check md5:{}'.format(hash_md5_check(s, r)))
+    elif t == 'sha1':
+        print('check sha1:{}'.format(hash_sha1_check(s, r)))
+    elif t == 'sha224':
+        print('check sha224:{}'.format(hash_sha224_check(s, r)))
+    elif t == 'sha256':
+        print('check sha256:{}'.format(hash_sha256_check(s, r)))
+    elif t == 'sha384':
+        print('check sha384:{}'.format(hash_sha384_check(s, r)))
+    elif t == 'sha512':
+        print('check sha512:{}'.format(hash_sha512_check(s, r)))
+    elif t == 'sha3_224':
+        print('check sha3_224:{}'.format(hash_sha3_224_check(s, r)))
+    elif t == 'sha3_256':
+        print('check sha3_256:{}'.format(hash_sha3_256_check(s, r)))
+    elif t == 'sha3_384':
+        print('check sha3_384:{}'.format(hash_sha3_384_check(s, r)))
+    elif t == 'sha3_512':
+        print('check sha3_512:{}'.format(hash_sha3_512_check(s, r)))
+    elif t == 'shake128':
+        print('check shake128:{}'.format(hash_shake128_check(s, r)))
+    elif t == 'shake256':
+        print('check shake256:{}'.format(hash_shake256_check(s, r)))
+    elif t == 'blake2b':
+        print('check blake2b:{}'.format(hash_blake2b_check(s, r)))
+    elif t == 'blake2s':
+        print('check blake2s:{}'.format(hash_blake2s_check(s, r)))
     else:
         print('Invalid hash algorithm. You can choose one of hash algorithm from support list.')
