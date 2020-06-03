2e6f2fc97bb1ec71ddf7545e9ebdbfaa829ff03c
AuroraShine <hyx1029535012@icloud.com>
2020-06-03 10:29:45 +0800

    [feature]Append test code

--- hash/hash_test.py
+++ hash/hash_test.py
@@ -60,6 +60,18 @@ class TestHash(unittest.TestCase):
         self.assertTrue(hash_check(s, r, 'sha384'))
         print('hash sha384 check pass')
 
+    def test_hash_sha512_encode(self):
+        s = 'hello,world!'
+        r = hash_gen(s, 'sha512')
+        print('hash sha512 encode:', r)
+
+    def test_hash_sha512_check(self):
+        s = 'hello,world!'
+        r = 'fa9edcfdaab7a4165f8d2593f04077d46aca957493e7e181ca479582d519a299d96730' \
+            '5294d5d46fb5e0158240441b94cd96510c2311bdc86870e5ebf3efe60c'
+        self.assertTrue(hash_check(s, r, 'sha512'))
+        print('hash sha512 check pass')
+
 
 if __name__ == '__main__':
     unittest.main()
