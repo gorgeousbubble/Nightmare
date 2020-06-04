98b34be7bf92194bb1fcc8d8f10b23e42b573f08
AuroraShine <hyx1029535012@icloud.com>
2020-06-04 10:29:43 +0800

    [feature]Append hash test code

--- hash/hash_test.py
+++ hash/hash_test.py
@@ -94,6 +94,19 @@ class TestHash(unittest.TestCase):
         self.assertTrue(hash_check(s, r, 'blake2s'))
         print('hash blake2s check pass')
 
+    def test_hash_shake128_encode(self):
+        s = 'hello,world!'
+        r = hash_gen(s, 'shake128')
+        print('hash shake128 encode:', r)
+
+    def test_hash_shake128_check(self):
+        s = 'hello,world!'
+        r = '12eaea2f1de368092b5418c6c5c8575a9a3490fd8d3c85a7fac818ccba01ea5d4526c2e6055b77b2b02a9b3b19f4b316dfc' \
+            '2c667dc18eda5522148e2949df7a3b6a25f37c9935e784077985dc95930d210b9aae7fb6a1f0491d1077177db27bdce8ce0' \
+            '3b5d5e7079abf4f30dd9aef53e518d4909e5421f8275ecb075e4de7543'
+        self.assertTrue(hash_check(s, r, 'shake128'))
+        print('hash shake128 check pass')
+
 
 if __name__ == '__main__':
     unittest.main()
