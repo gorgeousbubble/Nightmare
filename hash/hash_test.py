a4a0bff81e0a0a305a2f8a40ba043bf611516cf5
AuroraShine <hyx1029535012@icloud.com>
2020-06-04 10:36:35 +0800

    [feature]Append hash test code

--- hash/hash_test.py
+++ hash/hash_test.py
@@ -123,6 +123,17 @@ class TestHash(unittest.TestCase):
         self.assertTrue(hash_check(s, r, 'shake256'))
         print('hash shake256 check pass')
 
+    def test_hash_sha3_224_encode(self):
+        s = 'hello,world!'
+        r = hash_gen(s, 'sha3_224')
+        print('hash sha3_224 encode:', r)
+
+    def test_hash_sha3_224_check(self):
+        s = 'hello,world!'
+        r = 'be93af91ae2dd882018a39e0769933340315aff480d880fe71913177'
+        self.assertTrue(hash_check(s, r, 'sha3_224'))
+        print('hash sha3_224 check pass')
+
 
 if __name__ == '__main__':
     unittest.main()
