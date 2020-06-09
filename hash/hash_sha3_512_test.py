1f4a899cb45a68d3306b2c68279a6487fb1afe3e
AuroraShine <hyx1029535012@icloud.com>
2020-06-09 10:15:10 +0800

    [feature]Append test code

--- hash/hash_sha3_512_test.py
+++ hash/hash_sha3_512_test.py
@@ -12,6 +12,11 @@ class TestHashSHA3512(unittest.TestCase):
 
     def test_hash_sha3_512_check(self):
         s = 'hello,world!'
-        r = '4ed8dc6739e8edaa481d548e7f93d7a6c63d833a75bcfbd615fd932c97e6f357316e10d488778b12d2b2004341b641c54fa3a7942555bdcc1f672c3bfa578725'
+        r = '4ed8dc6739e8edaa481d548e7f93d7a6c63d833a75bcfbd615fd932c97e6f357316e10d488778b12d2b2004341b641c54fa3a794' \
+            '2555bdcc1f672c3bfa578725'
         self.assertTrue(hash_sha3_512_check(s, r))
         print('hash sha3_512 check pass')
+
+
+if __name__ == '__main__':
+    unittest.main()
