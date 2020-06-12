e425e594bcb84e01900abcc4a1944cddc282edd0
AuroraShine <hyx1029535012@icloud.com>
2020-06-12 10:30:09 +0800

    [feature]Append hash hmac md5

--- hash/hash_hmac.py
+++ hash/hash_hmac.py
@@ -11,3 +11,8 @@ def hash_hmac_md5(s, k):
 def hash_hmac_sha1(s, k):
     h = hmac.new(k.encode('utf-8'), s.encode('utf-8'), 'SHA1')
     return h.hexdigest()
+
+
+def hash_hmac_sha256(s, k):
+    h = hmac.new(k.encode('utf-8'), s.encode('utf-8'), 'SHA256')
+    return h.hexdigest()
