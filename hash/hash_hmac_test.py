08529e903549763b26a7343867af43781658d45e
AuroraShine <hyx1029535012@icloud.com>
2020-06-15 13:09:59 +0800

    [feature]Change function name

--- hash/hash_hmac_test.py
+++ hash/hash_hmac_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import unittest
-from .hash_hmac import hash_hmac_md5_encode
+from .hash_hmac import hash_hmac_md5_encode, hash_hmac_sha1_encode
 
 
 class TestHashHMAC(unittest.TestCase):
@@ -9,6 +9,11 @@ class TestHashHMAC(unittest.TestCase):
         s = 'hello,world!'
         r = hash_hmac_md5_encode(s, '')
         print('hash hmac md5 encode:', r)
+
+    def test_hash_hmac_sha1_encode(self):
+        s = 'hello,world!'
+        r = hash_hmac_sha1_encode(s, '')
+        print('hash hmac sha1 encode:', r)
 
 
 if __name__ == '__main__':
