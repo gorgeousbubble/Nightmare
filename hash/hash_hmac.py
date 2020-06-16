de70c71ebf16293d9baf554751bbb7485f44ab75
AuroraShine <hyx1029535012@icloud.com>
2020-06-16 10:07:16 +0800

    [feature]Append hash hmac code and test code

--- hash/hash_hmac.py
+++ hash/hash_hmac.py
@@ -16,3 +16,8 @@ def hash_hmac_sha1_encode(s, k):
 def hash_hmac_sha256_encode(s, k):
     h = hmac.new(k.encode('utf-8'), s.encode('utf-8'), 'SHA256')
     return h.hexdigest()
+
+
+def hash_hmac_sha512_encode(s, k):
+    h = hmac.new(k.encode('utf-8'), s.encode('utf-8'), 'SHA512')
+    return h.hexdigest()
--- hash/hash_hmac_test.py
+++ hash/hash_hmac_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import unittest
-from .hash_hmac import hash_hmac_md5_encode, hash_hmac_sha1_encode, hash_hmac_sha256_encode
+from .hash_hmac import hash_hmac_md5_encode, hash_hmac_sha1_encode, hash_hmac_sha256_encode, hash_hmac_sha512_encode
 
 
 class TestHashHMAC(unittest.TestCase):
@@ -19,6 +19,11 @@ class TestHashHMAC(unittest.TestCase):
         s = 'hello,world!'
         r = hash_hmac_sha256_encode(s, '')
         print('hash hmac sha256 encode:', r)
+
+    def test_hash_hmac_sha512_encode(self):
+        s = 'hello,world!'
+        r = hash_hmac_sha512_encode(s, '')
+        print('hash hmac sha512 encode:', r)
 
 
 if __name__ == '__main__':
