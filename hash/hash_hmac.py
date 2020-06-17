ad743afb1e890cac2c1f451a7f090b02d3e5d74f
AuroraShine <hyx1029535012@icloud.com>
2020-06-17 13:26:37 +0800

    [feature]Append encode function

--- hash/hash_hmac.py
+++ hash/hash_hmac.py
@@ -3,8 +3,18 @@
 import hmac
 
 
+def hash_hmac_md5(s, k):
+    h = hmac.new(k.encode('utf-8'), s.encode('utf-8'), 'MD5')
+    return h.hexdigest()
+
+
 def hash_hmac_md5_encode(s, k):
     h = hmac.new(k.encode('utf-8'), s.encode('utf-8'), 'MD5')
+    return h.hexdigest()
+
+
+def hash_hmac_sha1(s, k):
+    h = hmac.new(k.encode('utf-8'), s.encode('utf-8'), 'SHA1')
     return h.hexdigest()
 
 
@@ -13,8 +23,18 @@ def hash_hmac_sha1_encode(s, k):
     return h.hexdigest()
 
 
+def hash_hmac_sha256(s, k):
+    h = hmac.new(k.encode('utf-8'), s.encode('utf-8'), 'SHA256')
+    return h.hexdigest()
+
+
 def hash_hmac_sha256_encode(s, k):
     h = hmac.new(k.encode('utf-8'), s.encode('utf-8'), 'SHA256')
+    return h.hexdigest()
+
+
+def hash_hmac_sha512(s, k):
+    h = hmac.new(k.encode('utf-8'), s.encode('utf-8'), 'SHA512')
     return h.hexdigest()
 
 
