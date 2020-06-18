813c8dd2b3840044f79d0d84c39dfb0909b98178
AuroraShine <hyx1029535012@icloud.com>
2020-06-18 10:38:37 +0800

    [feature]Append hash hmac test code

--- hash/hash_hmac_test.py
+++ hash/hash_hmac_test.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import unittest
 from .hash_hmac import hash_hmac_md5_encode, hash_hmac_sha1_encode, hash_hmac_sha256_encode, hash_hmac_sha512_encode,\
-    hash_hmac_md5_check, hash_hmac_sha1_check, hash_hmac_sha256_check
+    hash_hmac_md5_check, hash_hmac_sha1_check, hash_hmac_sha256_check, hash_hmac_sha512_check
 
 
 class TestHashHMAC(unittest.TestCase):
@@ -40,6 +40,12 @@ class TestHashHMAC(unittest.TestCase):
         s = 'hello,world!'
         r = 'b3573c8ed2a62017f5503f06e78549fe1a55355c1e7e1f925bdb8f0b9c8a029f'
         self.assertTrue(hash_hmac_sha256_check(s, '', r))
+
+    def test_hash_hmac_sha512_check(self):
+        s = 'hello,world!'
+        r = 'e80ef886694e53473de1e01e328e40dd20289e2f7a61f4a93b036259343ee02a7d5026dffa10847077f4e8486d7c1ce20b697c7c' \
+            'e8d8decddf91bef3ff78f4c8'
+        self.assertTrue(hash_hmac_sha512_check(s, '', r))
 
 
 if __name__ == '__main__':
