d365fa36ad3bb20e0de8f59411f40af9888390b6
AuroraShine <hyx1029535012@icloud.com>
2020-07-03 10:18:03 +0800

    [improve]optimize code

--- cmds/__init__.py
+++ cmds/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import cmds
+from cmds.cmd_hash import parse_cmd_hash
 from cmds.cmd_help import parse_cmd_help
 from cmds.cmd_tcp import parse_cmd_tcp
 from cmds.cmd_udp import parse_cmd_udp
-from cmds.cmd_hash import parse_cmd_hash
